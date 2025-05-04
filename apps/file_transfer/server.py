c0c575c5d24ed069cca340b009e59188510dd0c5
Vitor Moreira RR <vitorrafael20111@gmail.com>
2025-05-04 19:31:41 -0300

    ipv6

--- apps/file_transfer/server.py
+++ apps/file_transfer/server.py
@@ -8,8 +8,16 @@ async def run_server_async(port, input_path, output_path):
     async def client_handler(reader, writer):
         await handle_connection_async(reader, writer, input_path, output_path)
 
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)  # <- ESSENCIAL para dual-stack
+    sock.bind(('::', port))  # Bind em todas interfaces (IPv6 e IPv4)
+    sock.listen(100)
+    sock.setblocking(False)
+
+    
     server = await asyncio.start_server(
-        client_handler, host="", port=port, family=socket.AF_UNSPEC, reuse_address=True
+        client_handler, sock=sock#host="", port=port, family=socket.AF_UNSPEC, reuse_address=True
     )
 
     addr = server.sockets[0].getsockname()
